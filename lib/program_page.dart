f69a8a5f373217ead1d0ff560d161186626f1c65
Cole W <cwhockey44@gmail.com>
2024-07-31 21:48:11 -0400

    nicer program day adding

--- lib/program_page.dart
+++ lib/program_page.dart
@@ -9,33 +9,43 @@ import 'package:flutter/material.dart';
 
 // void main() => runApp(new MaterialApp(home: MyList()));
 
+//program page, where user defines the overall program by days,
+// then excercises for each day with sets, rep range and notes
 class ProgramPage extends StatefulWidget {
   @override
   _MyListState createState() => _MyListState();
 }
 
+// this class contains the list view of expandable card tiles 
+// title is day title (eg. 'legs') and when expanded, leg excercises for that day show up
 class _MyListState extends State<ProgramPage> {
   int value = 0;
   List<TextEditingController> splitDaysTEC = [TextEditingController(), TextEditingController(), TextEditingController()];//TextEditingController();
   List<String> split = ["Push", "Pull", "Legs"];
+
+  // list of excercises at a given day
+  List<List<String>> excercises = [[], [], []];
+
+  // adds day to split
   _addItem() {
     setState(() {
       value = value + 1;
       splitDaysTEC.add(TextEditingController());
       split.add("New Day");
-      print(split.toString());
+      excercises.add([]);
+      //print(split.toString());
       //print(splitDaysTEC.toString());
     });
   }
 
   @override
+  // main scaffold, putting it all together
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        // program title
         title: TextFormField(
               //controller: splitDaysTEC,
-  
-            
             style: TextStyle(
               fontSize: 20,
             ),
@@ -50,73 +60,157 @@ class _MyListState extends State<ProgramPage> {
             ),
           ),
       ),
+
+      //list of day cards
       body: ListView.builder(
-          itemCount: split.length,
+          itemCount: split.length + 1,
           itemBuilder: (context, index) {
-    //print(index.toString());
-    //print(splitDaysTEC.length);
-        return Card(
-            child: Padding(
-            padding: EdgeInsets.only(
-                top: 8, left: 8.0, right: 8.0, bottom: 8.0),
-                child: ExpansionTile(
-                title: 
-                Row(
-                        //verticalDirection: VerticalDirection,
-                        children: [
-                          Expanded(
-                            child: ListTile(
-                        title: TextFormField(
+            //print(index.toString());
+            //print(splitDaysTEC.length);
+            // defines what each card will look like
+            if (index == split.length){
+              return Card(
+                
+                color: Colors.deepPurple,
+                child: InkWell(
+     
+                  splashColor: Colors.purple,
+                  borderRadius: BorderRadius.circular(16),
+                  onTap: () {
+                    _addItem();
+                  },
+                  child: SizedBox(
+                    width: double.infinity,
+                    height: 50.0,
+                    child: Icon(Icons.add),
+                  ),
+                ),
+              );
+            }
+            else{
+            return Card(
+              
+              child: Padding(
+                padding: EdgeInsets.only(
+                  top: 8, left: 8.0, right: 8.0, bottom: 8.0),
+                  child: ExpansionTile(
+                  title: 
+                  // row has day title, confirm update button, delete button
+                  // and excercise dropdown button
+                    Row(
+                      //verticalDirection: VerticalDirection,
+                      children: [
+                        Expanded(
+                          child: ListTile(
+                            title: TextFormField(
                               controller: splitDaysTEC[index],
                               decoration: InputDecoration(
                                 border: OutlineInputBorder(
                                     borderRadius: BorderRadius.all(Radius.circular(8))),
                                 hintText: split[index],
                               ),
-                            ),),
+                            ),
                           ),
+                        ),
+
+                        //confirm update button
+                        IconButton(onPressed: () {
+                          setState( () {
+                            split[index] = (splitDaysTEC[index].text);
+                            //closes keyboard
+                            FocusManager.instance.primaryFocus?.unfocus();
+                            });
+                          }, icon: Icon(Icons.check)
+                        ),
+
+                        // detete day button
+                        IconButton(onPressed: () {
+                          setState( () {
+                            value = value - 1;
+                            split.removeAt(index);
+                            excercises.removeAt(index);
+                            splitDaysTEC.removeAt(index);
+                          });
+                          }, icon: Icon(Icons.delete)
+                        ),
+                      ],
+                    ),
+
+                  // excercises for each day
+                  //this part is viewed after tile is expanded
+                    children: [
+                      // this needs to be done using listtiles
+                      for (int exc = 0; exc < excercises[index].length; exc++) 
+                      ExpansionTile(
+                        title: 
+                        Row(
+                          children: [
+                            Expanded(
+                              child: ListTile(
+                                title: TextFormField(
+                              //controller: splitDaysTEC[index],
+                                  decoration: InputDecoration(
+                                    border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.all(Radius.circular(8))),
+                                    hintText: split[index][exc],
+                                  ),
+                                ),
+                              ),
+                            ),
                           // SizedBox(
                           //   width: 1,
                           // ),
-                          IconButton(onPressed: () {
-                            setState( () {
-                              split[index] = (splitDaysTEC[index].text);
-                              FocusManager.instance.primaryFocus?.unfocus();
-                          });
-                          }, icon: Icon(Icons.check)),
+                            IconButton(onPressed: () {
+                              setState( () {
+                                split[index] = (splitDaysTEC[index].text);
+                                FocusManager.instance.primaryFocus?.unfocus();
+                              });
+                              }, icon: Icon(Icons.check)
+                            ),
 
-                          IconButton(onPressed: () {
-                            setState( () {
-                              value = value - 1;
-                              split.removeAt(index);
-                              splitDaysTEC.removeAt(index);
-                          });
-                          }, icon: Icon(Icons.delete)),
-                        ],
-                      ),
-
-                  children: [
-                    for (int exc = 0; exc < 3; exc++)ExpansionTile(
-                    title: Text(
-                      "runnerok",
-                      style: TextStyle(
-                        fontSize: 14
-                      ),
-                      ),
-                  children: <Widget>[
+                            IconButton(onPressed: () {
+                              setState( () {
+                                value = value - 1;
+                                split.removeAt(index);
+                                excercises.removeAt(index);
+                                splitDaysTEC.removeAt(index);
+                              });
+                              }, icon: Icon(Icons.delete)
+                            ),
+                            
+                          ],
+                        ),
+                      children: <Widget>[
                   
-                  Text('No belt, 0 RIR all sets. no safeties, thats for babies'),
+                      Text('No belt, 0 RIR all sets. no safeties, thats for babies'),
                   
-                ],
-              ),]
+                      ],
+                    ),
+                    Card(
+                
+                color: Colors.deepPurple,
+                child: InkWell(
+     
+                  splashColor: Colors.purple,
+                  borderRadius: BorderRadius.circular(16),
+                  onTap: () {
+                    //this needs to be done using listtiles
+                    excercises[index].add("new");
+                  },
+                  child: SizedBox(
+                    width: double.infinity,
+                    height: 50.0,
+                    child: Icon(Icons.add),
+                  ),
+                ),
               ),
-            ),
-          );
+                    ]
+                  
+                  ),
+                ),
+              );
+          }
     },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _addItem,
-        child: Icon(Icons.add),
       ),
     );
   }
